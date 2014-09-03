c7a00e7ce634551070cf7f0898cc0a451c756218
Nick Stefan <NickStefan12@gmail.com>
2014-09-03 16:34:39 -0700

    bootstrap.css styled the chat window scroll and input

--- public/js/sock-chat.js
+++ public/js/sock-chat.js
@@ -2,6 +2,12 @@ var socket = io();
 
 $(function(){
   
+  $('.messages').css('height',( $(window).height() / 2) );
+  
+  $('button').on('click',function(){
+    $('form').submit();
+  });
+  
   $('form').submit(function(e){
 
     socket.emit('chat message', $('#m').val());
@@ -12,7 +18,8 @@ $(function(){
   });
   
   socket.on('chat message', function(msg){
-    $('#messages').append($('<li>').text(msg));
+    $('.messages').append($('<li>').text(msg));
+    $('.messages').scrollTop( $('.messages')[0].scrollHeight );
   });
 
 });
